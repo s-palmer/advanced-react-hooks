8c0f86e260f58029f68be924182d6480a78e0df0
Sergei Palmer <zigpalmer@gmail.com>
2022-01-25 12:58:47 +0000

    useRef example 2

---
+++ src/components/UseRefExample2.jsx
@@ -0,0 +1,25 @@
+import { useRef, useState, useEffect } from "react";
+
+const UseRefExample2 = () => {
+  const renders = useRef(1);
+  const [name, setName] = useState('');
+  const prevName = useRef('')
+
+
+  useEffect(() => {
+    renders.current = renders.current + 1;
+    prevName.current = name
+  }, [name]);
+
+  return (
+    <div>
+      <h1>
+        Renders: {renders.current}
+      </h1>
+      <h2>Prev Name State: {prevName.current}</h2>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control mb-3" />
+    </div>
+  );
+};
+
+export default UseRefExample2;
